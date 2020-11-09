10708bf9dec5dc0773e7ca94c8888871ab04baa7
Jaemin Hong <hjm0901@gmail.com>
2020-11-09 15:24:45 +0900

    fix macros

--- src/main/g8/macros/src/main/scala/cs320/Macros.scala
+++ src/main/g8/macros/src/main/scala/cs320/Macros.scala
@@ -6,19 +6,19 @@ import scala.language.experimental.macros
 object Macros {
   def testImpl(c: Context)(f: c.Tree, v: c.Tree): c.Tree = {
     import c.universe._
-    q"""{
+    q"""(() => {
       import scala.reflect.runtime.universe._
       val code = normalize(show(reify{ $f }.tree))
       val value = normalize(show(reify{ $v }.tree))
       code should "be " ++ value in { assert($f == $v) }
-    }"""
+    })()"""
   }
 
   def test(f: Any, v: Any): Unit = macro testImpl
 
   def testExcImpl(c: Context)(f: c.Tree, msg: c.Tree): c.Tree = {
     import c.universe._
-    q"""{
+    q"""(() => {
       import scala.reflect.runtime.universe._
       val code = normalize(show(reify{ $f }.tree))
       val m = $msg
@@ -26,7 +26,7 @@ object Macros {
         val caught = intercept[PLError] { $f }
         assert(caught.msg.contains(m))
       }
-    }"""
+    })()"""
   }
 
   def testExc(f: Any, msg: String): Unit = macro testExcImpl
